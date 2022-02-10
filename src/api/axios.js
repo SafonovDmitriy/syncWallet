afeaaf2697b5bf9b5dfb5e3f48c3b343e56155f5
safonov_dmitriy <sdssds341@gmail.com>
2022-02-10 15:54:14 +0200

    [fix] clear package.json

--- src/api/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const instance = axios.create({
-  baseURL: process.env.REACT_APP_URL_SERVER,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  withCredentials: true,
-});
